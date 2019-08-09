f3e1050d4b84044c16bc502837a7ded7976b694a
Rahman Younus <rahman@uttily.com>
2019-08-09 18:13:11 +0100

    refactored logic to ask one question at a time and get fresh options based of last input

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ import { getOptionValues, getResults } from "./lib/scrapper";
 import { handleInput } from "./lib/cli"
 
 async function run() {
-    const options = await getOptionValues();
-    const input = await handleInput(options);
-    const results = await getResults(input);
+    // const options = await getOptionValues();
+    const input = await handleInput();
+    // const results = await getResults(input);
 
-    console.log({input, results});
+    // console.log({input, results});
 }
 
 run();
+
+
+// TODO:
+// 1. Ask first question
+// 2. Get count and furhter questions
+// 3. go to next question
+// 4. get count and further count
